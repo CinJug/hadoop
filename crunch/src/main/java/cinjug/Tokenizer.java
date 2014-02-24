1a14a43db4039ee2a4ee7cfb9f01609b4dff8098
Jason Hubbard <jasonahubbard@gmail.com>
2014-02-24 17:28:43 -0500

    add crunch base

---
+++ crunch/src/main/java/cinjug/Tokenizer.java
@@ -0,0 +1,23 @@
+package cinjug;
+
+import org.apache.crunch.DoFn;
+import org.apache.crunch.Emitter;
+
+import com.google.common.base.Splitter;
+
+
+/**
+ * Splits a line of text, filtering known stop words.
+ */
+public class Tokenizer extends DoFn<String, String> {
+
+	private static final Splitter SPLITTER = Splitter.onPattern("${symbol_escape}${symbol_escape}s+").omitEmptyStrings();
+
+	@Override
+	public void process(String line, Emitter<String> emitter) {
+		for (String word : SPLITTER.split(line)) {
+			emitter.emit(word);
+		}
+	}
+
+}
